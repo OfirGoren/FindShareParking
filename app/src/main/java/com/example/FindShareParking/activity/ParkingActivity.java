0a33d5adec90a773affa5edf3dff2c5ff489e459
Ofir goren <ofiir19@hotmail.com>
2021-07-23 16:31:15 +0300

    fourth commit

--- app/src/main/java/com/example/FindShareParking/activity/ParkingActivity.java
+++ app/src/main/java/com/example/FindShareParking/activity/ParkingActivity.java
@@ -348,7 +348,6 @@ public class ParkingActivity extends AppCompatActivity implements PostNotifyCall
                 if (isGranted) {
                     hideBottomNav();
                     displayNewPostFragment();
-                } else {
                 }
             });
 
